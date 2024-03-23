645a2a2a8b28faf40a3111209b808597017d78d3
Raquel Bulhões <138785661+Kelzinha000@users.noreply.github.com>
2024-03-23 09:17:30 -0300

    Add files via upload

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return(
+        <footer className="meuFooter">
+            <p>&copy; Todos os Direitos Reservados</p>
+        </footer>
+    )
+}
+
+export default Footer; 
---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+     return(
+        <header style={{"backgroundColor":"gray"}}>
+            <h1>MeuSite</h1>
+            <ul>
+                <li>Login</li>
+                <li>Registrar</li>
+                <li>Sobre</li>
+            </ul>
+        </header>
+     )
+}
+export default Header;
